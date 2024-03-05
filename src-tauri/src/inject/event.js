d2a893dfa8fc443a7489d2451d93366699b4d375
hades0932 <52244757+hades0932@users.noreply.github.com>
2024-03-05 18:12:23 +0800

    Update event.js
    test1
    
    Signed-off-by: hades0932 <52244757+hades0932@users.noreply.github.com>

--- src-tauri/src/inject/event.js
+++ src-tauri/src/inject/event.js
@@ -231,6 +231,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const baseUrl = window.location.origin + window.location.pathname;
       const hrefUrl = new URL(url, baseUrl);
       tauri.shell.open(hrefUrl.href);
+      return;
     }
     // Call the original window.open function to maintain its normal functionality.
     return originalWindowOpen.call(window, url, name, specs);
